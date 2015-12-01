80a54d161b885b100d936e07f713b808566f542a
Jointome <rcoutinhocosta@msn.com>
2015-12-01 13:25:05 +0000

    canvas

--- Mines/vsgame.js
+++ Mines/vsgame.js
@@ -2,17 +2,20 @@
 var user = "";
 var pass = "";
 var URL =  "http://twserver.alunos.dcc.fc.up.pt:8000/"
-var curPlayer = '';
 var game_num = null;
 var game_key = null;
 var opponent = false;
+var quemJoga = null;
 var array = ["beginner","intermediate","expert"];
+var bombsfound=0;
+var maxbombs;
+var enebombsfound=0;
 
 
 //var players = ["player1","player2"];
 
 function login(){
-	online = true;
+    online = true;
     user = myForm.fname.value;
     pass = myForm.password.value;
     var loginInfo = JSON.stringify({'name': user, 'pass':pass});
@@ -66,10 +69,9 @@ function showvsgame(diff){
 }
 
 function createvsgame(){
-    var nbombs = diff[difficulty].nbombs;
+    var nbombs = maxbombs = diff[difficulty].nbombs;
     nrow = diff[difficulty].nrow;
     ncol = diff[difficulty].ncol;
-//    curPlayer = players[Math.floor((Math.random() * 1))];
 
     board = [ ncol ];
     auxboard = [ ncol ];
@@ -87,9 +89,11 @@ function createvsgame(){
 
 function vsPlayer(){
     createvsgame();
+    percent();
+    percent2();
     online = true;
     var table = document.getElementById(tableid[difficulty]);
-    var quemJoga = null;
+    quemJoga = null;
     var params = {'name':user, 'pass': pass, 'level':array[difficulty], 'group':37};
     var req = new XMLHttpRequest();
     req.open("POST", URL+"join", true);
@@ -203,23 +207,32 @@ var params = {'name': user, 'game': game_num, 'key': game_key, 'row': row, 'col'
 }
 
 function expandeX(array, table){
-	for(var i = 0; i < array.length; i++){
-		if(!(array[i][2] === 0 || array[i][2]== -1) ){
-		table.rows[array[i][0] - 1].cells[array[i][1] - 1].appendChild(document.createTextNode(array[i][2]));
-		}
-		if(array[i][2] == -1)
-			table.rows[array[i][0] - 1].cells[array[i][1] - 1].className = tdimg[difficulty].blackbomb;
-		else{
-			table.rows[array[i][0] - 1].cells[array[i][1] - 1].className = tdimg[difficulty].read;
-			if(array[i][2] === 1)
-		 	   table.rows[array[i][0] - 1].cells[array[i][1] - 1].classList.add('blue');
-			else if(array[i][2] === 2)
-		 	   table.rows[array[i][0] - 1].cells[array[i][1] - 1].classList.add('green');
-			else
-		    	table.rows[array[i][0] - 1].cells[array[i][1] - 1].classList.add('red');
-		}
-
+    for(var i = 0; i < array.length; i++){
+	if(!(array[i][2] === 0 || array[i][2]== -1) ){
+	    table.rows[array[i][0] - 1].cells[array[i][1] - 1].appendChild(document.createTextNode(array[i][2]));
 	}
+	if(array[i][2] == -1){
+	    table.rows[array[i][0] - 1].cells[array[i][1] - 1].className = tdimg[difficulty].blackbomb;
+	    if(quemJoga == user){
+	    bombsfound++;
+		draw();
+	    }
+	    else{
+	    enebombsfound++;
+	    draw2();
+	    }
+	}
+	else{
+	    table.rows[array[i][0] - 1].cells[array[i][1] - 1].className = tdimg[difficulty].read;
+	    if(array[i][2] === 1)
+		table.rows[array[i][0] - 1].cells[array[i][1] - 1].classList.add('blue');
+	    else if(array[i][2] === 2)
+		table.rows[array[i][0] - 1].cells[array[i][1] - 1].classList.add('green');
+	    else
+		table.rows[array[i][0] - 1].cells[array[i][1] - 1].classList.add('red');
+	}
+	
+    }
 }
 
 function callRanking(){
@@ -278,4 +291,201 @@ function quitGame(){
 	}
     else signOut();
 	
+}
+
+var canvas ;
+var ctx;
+//dimensions
+var W;
+var H;
+//Variables
+var degrees;
+var new_degrees;
+var difference;
+var color;
+var bgcolor;
+var text;
+var animation_loop, redraw_loop;
+function percent(){
+    //canvas initialization
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext("2d");
+    //dimensions
+    W = canvas.width;
+    H = canvas.height;
+    //Variables
+    degrees = 0;
+    new_degrees = 0;
+    difference = 0;
+    color = "lightgreen"; //green looks better to me
+    bgcolor = "#222";
+    text;
+    animation_loop, redraw_loop;
+    init();
+    draw();
+    animate_to();
+}
+
+function init()
+{
+    //Clear the canvas everytime a chart is drawn
+    ctx.clearRect(0, 0, W, H);
+    
+    //Background 360 degree arc
+    ctx.beginPath();
+    ctx.strokeStyle = bgcolor;
+    ctx.lineWidth = 30;
+    ctx.arc(W/2, H/2, 100, 0, Math.PI*2, false); //you can see the arc now
+    ctx.stroke();
+    
+    //gauge will be a simple arc
+    //Angle in radians = angle in degrees * PI / 180
+    var radians = degrees * Math.PI / 180;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 30;
+    //The arc starts from the rightmost end. If we deduct 90 degrees from the angles
+    //the arc will start from the topmost end
+    ctx.arc(W/2, H/2, 100, 0 - 90*Math.PI/180, radians - 90*Math.PI/180, false); 
+    //you can see the arc now
+    ctx.stroke();
+    
+    //Lets add the text
+    ctx.fillStyle = color;
+    ctx.font = "50px bebas";
+    text = Math.floor(degrees/360*100) + "%";
+    //Lets center the text
+    //deducting half of text width from position x
+    text_width = ctx.measureText(text).width;
+    //adding manual value to position y since the height of the text cannot
+    //be measured easily. There are hacks but we will keep it manual for now.
+    ctx.fillText(text, W/2 - text_width/2, H/2 + 15);
+}
+
+function draw()
+{
+    //Cancel any movement animation if a new chart is requested
+    if(typeof animation_loop != undefined) clearInterval(animation_loop);
+    
+    //random degree from 0 to 360
+    new_degrees = ((bombsfound * 360)/(maxbombs/2));
+    difference = new_degrees - degrees;
+    //This will animate the gauge to new positions
+    //The animation will take 1 second
+    //time for each frame is 1sec / difference in degrees
+    animation_loop = setInterval(animate_to, 1000/difference);
+}
+
+//function to make the chart move to new degrees
+function animate_to()
+{
+    //clear animation loop if degrees reaches to new_degrees
+    if(degrees == new_degrees) 
+	clearInterval(animation_loop);
+    
+    if(degrees < new_degrees)
+	degrees++;
+    else
+	degrees--;
+    
+    init();
+}
+
+var canvas2 ;
+var ctx2;
+//dimensions
+var W2;
+var H2;
+//Variables
+var degrees2;
+var new_degrees2;
+var difference2;
+var color2;
+var bgcolor2;
+var text2;
+var animation_loop2, redraw_loop2;
+
+function percent2(){
+    //canvas initialization
+    canvas2 = document.getElementById('canvasadv');
+    ctx2 = canvas2.getContext("2d");
+    //dimensions
+    W2 = canvas2.width;
+    H2 = canvas2.height;
+    //Variables
+    degrees2 = 0;
+    new_degrees2 = 0;
+    difference2 = 0;
+    color2 = "red";
+    bgcolor2 = "#222";
+    text2;
+    animation_loop2, redraw_loop2;
+    init2();
+    draw2();
+    animate_to2();
+}
+
+function init2()
+{
+    //Clear the canvas everytime a chart is drawn
+    ctx2.clearRect(0, 0, W, H);
+    
+    //Background 360 degree arc
+    ctx2.beginPath();
+    ctx2.strokeStyle = bgcolor2;
+    ctx2.lineWidth = 30;
+    ctx2.arc(W2/2, H2/2, 100, 0, Math.PI*2, false); //you can see the arc now
+    ctx2.stroke();
+    
+    //gauge will be a simple arc
+    //Angle in radians = angle in degrees * PI / 180
+    var radians2 = degrees2 * Math.PI / 180;
+    ctx2.beginPath();
+    ctx2.strokeStyle = color2;
+    ctx2.lineWidth = 30;
+    //The arc starts from the rightmost end. If we deduct 90 degrees from the angles
+    //the arc will start from the topmost end
+    ctx2.arc(W2/2, H2/2, 100, 0 - 90*Math.PI/180, radians2 - 90*Math.PI/180, false); 
+    //you can see the arc now
+    ctx2.stroke();
+    
+    //Lets add the text
+    ctx2.fillStyle = color2;
+    ctx2.font = "50px bebas";
+    text2 = Math.floor(degrees2/360*100) + "%";
+    //Lets center the text
+    //deducting half of text width from position x
+    text_width2 = ctx2.measureText(text2).width;
+    //adding manual value to position y since the height of the text cannot
+    //be measured easily. There are hacks but we will keep it manual for now.
+    ctx2.fillText(text2, W2/2 - text_width2/2, H2/2 + 15);
+}
+
+function draw2()
+{
+    //Cancel any movement animation if a new chart is requested
+    if(typeof animation_loop2 != undefined) clearInterval(animation_loop2);
+    
+    //random degree from 0 to 360
+    new_degrees2 = ((enebombsfound * 360)/(maxbombs/2));
+    difference2 = new_degrees2 - degrees2;
+    //This will animate the gauge to new positions
+    //The animation will take 1 second
+    //time for each frame is 1sec / difference in degrees
+    animation_loop2 = setInterval(animate_to2, 1000/difference2);
+}
+
+//function to make the chart move to new degrees
+function animate_to2()
+{
+    //clear animation loop if degrees reaches to new_degrees
+    if(degrees2 == new_degrees2) 
+	clearInterval(animation_loop2);
+    
+    if(degrees2 < new_degrees2)
+	degrees2++;
+    else
+	degrees2--;
+    
+    init2();
 }
